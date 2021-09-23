a017e74109240b9fb9b3ca8525c9d33add847cc2
RayKilburg <rkilburg42@gmail.com>
2021-09-22 23:18:25 -0400

    add, remove, mark complete

---
+++ script.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const TodosUL = document.getElementById('todos')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addTodo()
+})
+
+function addTodo(todo) {
+    let todoText = input.value
+
+    if(todo) {
+        todoText = todo.text
+    }
+
+    if(todoText) {
+        const todoEl = document.createElement('li')
+        if(todo && todo.completed) {
+            todoEl.classList.add('completed')
+        }
+
+        todoEl.innerText = todoText
+
+        // toggle completed
+        todoEl.addEventListener('click', () => todoEl.classList.toggle('completed'))
+
+        // delete todo
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+
+            todoEl.remove()
+        })
+
+        TodosUL.appendChild(todoEl)
+
+        input.value = ''
+    }
+}
